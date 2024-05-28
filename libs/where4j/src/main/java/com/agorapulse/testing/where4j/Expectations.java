eafa357087f03874a6f21ef304c598440b99d7ad
musketyr <vladimir@orany.cz>
2024-05-28 08:37:17 +0200

    added stream and iterable support

--- libs/where4j/src/main/java/com/agorapulse/testing/where4j/Expectations.java
+++ libs/where4j/src/main/java/com/agorapulse/testing/where4j/Expectations.java
@@ -3,9 +3,11 @@ package com.agorapulse.testing.where4j;
 import com.agorapulse.testing.where4j.dsl.Headers3;
 import com.agorapulse.testing.where4j.dsl.Row3;
 import com.agorapulse.testing.where4j.dsl.Where3;
+import com.agorapulse.testing.where4j.dsl.Zip3;
 import org.junit.jupiter.api.DynamicContainer;
 
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public interface Expectations extends Iterable<DynamicContainer> {
 
@@ -20,6 +22,28 @@ public interface Expectations extends Iterable<DynamicContainer> {
     @SafeVarargs
     static <A, B, C> Where3<A, B, C> forEach(Headers3 headers, Row3<A, B, C> first, Row3<A, B, C> ... rest) {
         return new Where3<>(headers, first, Arrays.asList(rest));
+    }
+
+    static <A, B, C> Where3<A, B, C> forEach(
+        String headerA,
+        Iterable<A> valuesA,
+        String headerB,
+        Iterable<B> valuesB,
+        String headerC,
+        Iterable<C> valuesC
+    ) {
+        return new Where3<>(header(headerA, headerB, headerC), new Zip3<>(valuesA.iterator(), valuesB.iterator(), valuesC.iterator()));
+    }
+
+    static <A, B, C> Where3<A, B, C> forEach(
+        String headerA,
+        Stream<A> valuesA,
+        String headerB,
+        Stream<B> valuesB,
+        String headerC,
+        Stream<C> valuesC
+    ) {
+        return new Where3<>(header(headerA, headerB, headerC), new Zip3<>(valuesA.iterator(), valuesB.iterator(), valuesC.iterator()));
     }
 
 }
